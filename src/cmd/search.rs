5df9718301445181669b3e095bd4691fb165f788
Daniel Imfeld <daniel@imfeld.dev>
2023-05-11 13:21:19 -0700

    Add score threhold and show search duration

--- src/cmd/search.rs
+++ src/cmd/search.rs
@@ -17,17 +17,32 @@ pub struct SearchArgs {
         help = "The maximum Levenshtein distance to search for"
     )]
     pub fuzzy: u32,
+
+    #[clap(
+        short = 's',
+        long = "score-threshold",
+        default_value_t = 0.7,
+        help = "The minimum score, using Jaccard trigram similarity, when performing fuzzy search"
+    )]
+    pub score_threshold: f32,
 }
 
 pub fn run(base_dir: &Path, _files: Files, args: SearchArgs) -> Result<()> {
     let dir = base_dir.join("index");
     let index = umls::index::Index::new(&dir)?;
 
+    let start_time = std::time::Instant::now();
     if args.fuzzy == 0 {
         match index.search(&args.word)? {
             Some(w) => {
+                let duration = start_time.elapsed();
                 let concept = index.concept_id(w);
-                println!("Found {} - {}", concept.cui, concept.preferred_name)
+                println!(
+                    "Found ({}us) {} - {}",
+                    duration.as_micros(),
+                    concept.cui,
+                    concept.preferred_name
+                )
             }
             None => println!("Not found"),
         }
@@ -37,10 +52,14 @@ pub fn run(base_dir: &Path, _files: Files, args: SearchArgs) -> Result<()> {
         while let Some((s, id, _)) = output.next() {
             let found = std::str::from_utf8(s)?.to_string();
             let score = jaccard_trigram_distance(&args.word, &found);
-            results.push((score, id, found));
+            if score >= args.score_threshold {
+                results.push((score, id, found));
+            }
         }
 
         results.sort_by(|(ascore, _, _), (bscore, _, _)| bscore.total_cmp(ascore));
+        let duration = start_time.elapsed();
+        println!("Search completed in {}us", duration.as_micros());
 
         if results.is_empty() {
             println!("No results found");
